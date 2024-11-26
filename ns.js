d6e87ed4a246bf4639ffa261b33cb31bbcc2a9c8
leohcwang <leohcwang@tencent.com>
2024-11-26 21:24:19 +0800

    --story=更改主题

--- ns.js
+++ ns.js
@@ -3,7 +3,7 @@ module.exports = {
         "bot-docs.io": "bot-docs.github.io",
         /* start */
         "jekyll-theme-docsify": "bot-docs.github.io",
-        "alembic-jekyll-theme": "bot-docs.github.io",
+        "clearpathrobotics/jekyll-rtd-theme": "bot-docs.github.io",
     },
     txt: {
         "bot-docs.io": "google-site-verification=X-KVlK1E2ZRkqgO3UzHYn4Fm26gUlZY132HREO4lkmM",
